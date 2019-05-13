05c2fafb6ada908aeb1309382eceebd667f8094a
Elias Pitschmann <elias.pitschmann@gmx.de>
2019-05-13 18:38:39 +0200

    Dokumentation aktualisiert

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ def main(*args, **kwargs):
     startet das Programm neu.
     """
 
-    action1 = input("Woher soll der zu prüfende Text stammen?\n"
+    action1 = input("Woher soll der zu pruefende Text stammen?\n"
                         "0: Aus Datei einlesen\n"
                         "1: String eingeben\n"
-                        "Mit 'help' lässt sich ein Hilfetext ausgeben.\n")
+                        "Mit 'help' laesst sich ein Hilfetext ausgeben.\n")
     if action1 == "0":
-        print("Bitte eine Datei im Explorer auswählen...")
-        path = tkf.askopenfilename(filetypes=[('Alle Dateien', '*')], title="Quelle für Text auswählen")
+        print("Bitte eine Datei im Explorer auswaehlen...")
+        path = tkf.askopenfilename(filetypes=[('Alle Dateien', '*')], title="Quelle fuer Text auswaehlen")
         t = open(path, 'r').read().replace('\n', '')
     elif action1 == "1":
         t = input("Text:\n")
@@ -41,10 +41,10 @@ def main(*args, **kwargs):
     action2 = input("Ok! Woher soll das gesuchte Pattern stammen?\n"
                         "0: Aus Datei einlesen\n"
                         "1: String eingeben\n"
-                        "Mit 'help' lässt sich ein Hilfetext ausgeben.\n")
+                        "Mit 'help' laesst sich ein Hilfetext ausgeben.\n")
     if action2 == "0":
-        print("Bitte eine Datei im Explorer auswählen...")
-        path = tkf.askopenfilename(filetypes=[('Alle Dateien', '*')], title="Quelle für Muster auswählen")
+        print("Bitte eine Datei im Explorer auswaehlen...")
+        path = tkf.askopenfilename(filetypes=[('Alle Dateien', '*')], title="Quelle fuer Muster auswaehlen")
         p = open(path, 'r').read().replace('\n', '')
     elif action2 == "1":
         p = input("Muster:\n")
@@ -54,13 +54,13 @@ def main(*args, **kwargs):
         print("Eingabe konnte nicht gelesen werden. Programm wird neugestartet.\n")
         main()
 
-    action3 = input("Ok! Welcher Algorithmus soll für die Suche verwendet werden?\n"
+    action3 = input("Ok! Welcher Algorithmus soll fuer die Suche verwendet werden?\n"
                         "0: Naiver Algorithmus\n"
                         "1: Rabin-Karp-Algorithmus\n"
                         "2: Knuth-Morris-Pratt-Algorithmus\n"
                         "3: Boyer-Moore-Algorithmus\n"
                         "4: Alle 4 Algorithmen\n"
-                        "Mit 'help' lässt sich ein Hilfetext ausgeben.\n")
+                        "Mit 'help' laesst sich ein Hilfetext ausgeben.\n")
     if action3 == "0":
         naiv(t, p)
     elif action3 == "1":
@@ -84,17 +84,17 @@ def main(*args, **kwargs):
 def help():
     print(
         "*** AMBI-Praktikum Aufgabe 1 - Bediedungsanleitung ***\n\n"
-        "\tMit diesem Programm können zwei Zeichenketten mit verschiedenen\n"
+        "\tMit diesem Programm koennen zwei Zeichenketten mit verschiedenen\n"
         "\tAlgorithmen verglichen werden\n"
-        "\tZeichenketten können entweder als String oder Datei übergeben werden.\n"
+        "\tZeichenketten koennen entweder als String oder Datei uebergeben werden.\n"
         "\tReihenfolge der Eingaben:\n"
-        "\t\t1. Wählen Sie eine Eingabemethode für den zu überprüfenden Text\n"
+        "\t\t1. Waehlen Sie eine Eingabemethode fuer den zu ueberpruefenden Text\n"
         "\t\t(String oder .fasta Datei) mit den Ziffern 0, 1\n"
-        "\t\t2. Wählen Sie eine .fasta Datei oder geben Sie einen String ein\n"
-        "\t\t3. Wählen Sie eine Eingabemethode für das gesuchte Pattern\n"
+        "\t\t2. Waehlen Sie eine .fasta Datei oder geben Sie einen String ein\n"
+        "\t\t3. Waehlen Sie eine Eingabemethode fuer das gesuchte Pattern\n"
         "\t\t(String oder Pattern) mit den Ziffern 0, 1\n"
-        "\t\t4. Wählen Sie einen Algorithmus für die Auswertung.\n"
-        "\t\tDafür stehen zur Verfügung:\n"
+        "\t\t4. Waehlen Sie einen Algorithmus fuer die Auswertung.\n"
+        "\t\tDafuer stehen zur Verfuegung:\n"
         "\t\t\t0: Naiver Algorithmus\n"
         "\t\t\t1: Rabin-Karp-Algorithmus\n"
         "\t\t\t2: Knuth-Morris-Pratt-Algorithmus\n"
@@ -126,7 +126,7 @@ def naiv(text, pattern):
         # Pruefe, ob pattern mit Verschiebung s im text auftaucht
         ident = 0
         for i in range(0, m):
-            c_schritte += 1 # nachfolgenden Vergleich zählen
+            c_schritte += 1 # nachfolgenden Vergleich zaehlen
             if pattern[i] == text[s+i]:
                 ident += 1
             else:
@@ -185,7 +185,7 @@ def rabin_karp(text, pattern, d):
         if p == t:
             ident = 0
             for i in range(0, m):
-                c_schritte += 1 # nachfolgenden Vergleich zählen
+                c_schritte += 1 # nachfolgenden Vergleich zaehlen
                 if pattern[i] == text[s+i]:
                     ident += 1
                 else:
@@ -234,7 +234,7 @@ def knuth_morris_pratt(text, pattern):
     """
     Funktioniert aehnlich wie der Ansatz mittels DFA. Nutzt Enthaltensein des Patterns in sich selbst aus, um bessere
     Erhoehungen des Shifts s zu erreichen. Liest dafuer den gesamten Text von links nach rechts.
-    :param text: zu ueberpruefendes Pattern, string
+    :param text: zu ueberpruefender Text, string
     :param pattern: gesuchtes Pattern, string
     :Ausgabe: der Algorithmus gibt die Verschiebungen fuer alle gefunden Pattern im Text an.
     Ausserdem wird die Anzahl der Funde und die benoetigte Anzahl der Vergleiche von Text und Pattern
@@ -258,7 +258,7 @@ def knuth_morris_pratt(text, pattern):
         while q > 0 and pattern[q] != text[i]:
             # Wie weit muss q reduziert werden? Das steht an der entsprechenden Stelle in __pi
             q = __pi[q - 1]
-        c_schritte += 1 # den nachfolgenden Vergleich zählen
+        c_schritte += 1 # den nachfolgenden Vergleich zaehlen
         if pattern[q] == text[i]:
             # Ein weiteres Zeichen wurde gematcht
             q = q + 1
@@ -310,7 +310,7 @@ def compute_good_suffix_function(pattern, m):
     pi2 = compute_prefix_function(pattern2)
     # Initialisierung der Liste
     __gamma = []
-    # Ermittle Einträge gemaess Rechnung aus der Vorlesung
+    # Ermittle Eintraege gemaess Rechnung aus der Vorlesung
     for j in range(0, m):
         __gamma.append(m - __pi[m-1])
     for l in range(0, m):
@@ -320,14 +320,14 @@ def compute_good_suffix_function(pattern, m):
     return __gamma
 
 
-def boyer_moore(t, p, sigma):
+def boyer_moore(text, pattern, sigma):
     """
     Der Algorithmus funktioniert wie der naive Algorithmus, nur dass er das Muster jeweils
     von rechts nach links liest und bei Mismatches durch Anwendung von Heuristiken bessere
-    Erhöhungen des Shifts s als 1 erzielt. Die Heuristiken heissen bad-character und good-suffix.
-    :param t:
-    :param p:
-    :param sigma:
+    Erhoehungen des Shifts s als 1 erzielt. Die Heuristiken heissen bad-character und good-suffix.
+    :param text: zu ueberpruefender Text, string
+    :param pattern: gesuchtes Pattern, string
+    :param sigma: Alphabet des Texts
     :Ausgabe: der Algorithmus gibt die Verschiebungen fuer alle gefunden Pattern im Text an.
     Ausserdem wird die Anzahl der Funde und die benoetigte Anzahl der Vergleiche von Text und Pattern
     gezaehlt und die Laufzeit gemessen und ausgegeben.
@@ -338,20 +338,20 @@ def boyer_moore(t, p, sigma):
 
     c_schritte = 0
     c_funde = 0
-    n = len(t)
-    m = len(p)
+    n = len(text)
+    m = len(pattern)
     # Woerterbuch fuer bad-character Heuristik
-    lam = compute_last_occurence_function(p, m, sigma)
+    lam = compute_last_occurence_function(pattern, m, sigma)
     # Liste fuer good-suffix Heuristik
-    __gamma = compute_good_suffix_function(p, m)
+    __gamma = compute_good_suffix_function(pattern, m)
     s = 0
-    # es gibt n - m + 1 mögliche shifts
+    # es gibt n - m + 1 moegliche shifts
     while s <= n - m:
         # durchlaufe das Muster von hinten nach vorne
         j = m - 1
-        c_schritte += 1 # nachfolgenden Vergleich zählen
-        while j >= 0 and p[j] == t[s + j]:
-            c_schritte += 1 # Vergleich für die Bed. des nächsten Durchlaufs
+        c_schritte += 1 # nachfolgenden Vergleich zaehlen
+        while j >= 0 and pattern[j] == text[s + j]:
+            c_schritte += 1 # Vergleich fuer die Bed. des naechsten Durchlaufs
             j -= 1
         if j == -1:
             c_schritte -= 1 # While-Bed ist schon an j>=0 gescheitert
@@ -360,7 +360,7 @@ def boyer_moore(t, p, sigma):
             s += __gamma[0]
             c_funde += 1
         else:
-            # Ermittlung, welche der beiden Heuristiken die größere Verschiebung liefert
+            # Ermittlung, welche der beiden Heuristiken die groessere Verschiebung liefert
             s += max(__gamma[j], j - lam[t[s + j]])
 
     # Messung der verbrauchten Zeit
